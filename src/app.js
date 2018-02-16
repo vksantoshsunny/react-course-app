dbd83eae19a553d19b2dcc7d407beea20d58e94f
vksantoshsunny <vksantoshsunny@gmail.com>
2018-02-17 11:04:08 +1300

    Components files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,96 @@
 import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Components/Header';
+import AddOption from './Components/AddOption';
+import Options from './Components/Options';
+import Option from './Components/Option';
+import Action from './Components/Action';
 
-console.log(validator.isEmail('touch'));
+class IndecisionApp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+      this.handlePick = this.handlePick.bind(this);
+      this.handleAddOption = this.handleAddOption.bind(this);
+      this.handleDeleteOption = this.handleDeleteOption.bind(this);
+      this.state = {
+        options: []
+      };
+    }
+    componentDidMount() {
+      try {
+        const json = localStorage.getItem('options');
+        const options = JSON.parse(json);
+  
+        if (options) {
+          this.setState(() => ({ options }));
+        }
+      } catch (e) {
+        // Do nothing at all
+      }
+    }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+        const json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+      }
+    }
+    componentWillUnmount() {
+      console.log('componentWillUnmount');
+    }
+    handleDeleteOptions() {
+      this.setState(() => ({ options: [] }));
+    }
+    handleDeleteOption(optionToRemove) {
+      this.setState((prevState) => ({
+        options: prevState.options.filter((option) => optionToRemove !== option)
+      }));
+    }
+    handlePick() {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      alert(option);
+    }
+    handleAddOption(option) {
+      if (!option) {
+        return 'Enter valid value to add item';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exists';
+      }
+  
+      this.setState((prevState) => ({
+        options: prevState.options.concat(option)
+      }));
+    }
+    render() {
+      const subtitle = 'Put your life in the hands of a computer';
+  
+      return (
+        <div>
+          <Header subtitle={subtitle} />
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+        </div>
+      );
+    }
+  }
+  
+
+  Header.defaultProps = {
+    title: 'Indecision'
+  };
+  
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
